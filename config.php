94667e189936ad971348646cd363e40ef78bb320
Inna Suknovalnik <isuknovalnik@gmail.com>
2021-01-23 17:07:13 +0700

    Правки

--- config.php
+++ config.php
@@ -1,3 +1,6 @@
 <?php
 date_default_timezone_set('Asia/Barnaul');
 // - date_default_timezone_set('Europe/Moscow');
+
+$con = mysqli_connect("localhost", "root", "","readme");
+mysqli_set_charset($con, "utf8");
--- index.php
+++ index.php
@@ -4,18 +4,35 @@ require_once 'util/helpers.php';
 require_once 'tmp-data.php';
 require_once 'util/functions.php';
 
-$con = mysqli_connect("localhost", "root", "","readme");
-mysqli_set_charset($con, "utf8");
 
 if (!$con) {
     $error = mysqli_connect_error();
-    $page_content = '<br><br><br><div>Ошибка ' . $error . '</div><br><br><br>';
+    $page_content = include_template('error', [
+        'error' => $error
+    ]);
+
 } else {
-    $sql = "SELECT type_name, icon_class FROM posts_types";
+    $sql = "SELECT type_name,
+                   icon_class
+              FROM posts_types";
 
     if ($res = mysqli_query($con, $sql)) {
         $posts_types = mysqli_fetch_all($res, MYSQLI_ASSOC);
-        $sql = "SELECT post_date, u.user_name, u.avatar, t.type_name, t.icon_class, title, content, content_add, views_total FROM posts p JOIN users u ON p.user_id = u.id JOIN posts_types t ON p.type_id = t.id ORDER BY views_total DESC";
+        $sql = "SELECT post_date,
+                       u.user_name,
+                       u.avatar,
+                       t.type_name,
+                       t.icon_class,
+                       title,
+                       content,
+                       content_add,
+                       views_total
+                  FROM posts p
+                  JOIN users u
+                    ON p.user_id = u.id
+                  JOIN posts_types t
+                    ON p.type_id = t.id
+              ORDER BY views_total DESC";
         if ($res = mysqli_query($con, $sql)) {
             $popular_posts = mysqli_fetch_all($res, MYSQLI_ASSOC);
             $posts_content = include_template('posts', [
@@ -26,11 +43,15 @@ if (!$con) {
             ]);
         } else {
             $error = mysqli_error();
-            $page_content = '<br><br><br><div>Ошибка ' . $error . '</div><br><br><br>';
+            $page_content = include_template('error', [
+                'error' => $error
+            ]);
         }
     } else {
         $error = mysqli_error();
-        $page_content = '<br><br><br><div>Ошибка ' . $error . '</div><br><br><br>';
+        $page_content = include_template('error', [
+            'error' => $error
+        ]);
     }
 }
 
