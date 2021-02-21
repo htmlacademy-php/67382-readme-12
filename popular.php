cd7d38dafdbf07e2f083d33b1fc603dcf0286701
Inna Suknovalnik <isuknovalnik@gmail.com>
2021-02-21 18:02:50 +0700

    Запросы к табл. постов в отдельные функции

--- popular.php
+++ popular.php
@@ -2,6 +2,7 @@
 require_once 'util/helpers.php';
 require_once 'tmp-data.php';
 require_once 'util/functions.php';
+require_once 'util/db_functions.php';
 require_once 'init.php';
 
 $sql = "SELECT * FROM posts_types";
@@ -9,58 +10,18 @@ $sql = "SELECT * FROM posts_types";
 if ($res = mysqli_query($con, $sql)) {
     $posts_types = mysqli_fetch_all($res, MYSQLI_ASSOC);
     $filters_type = (int) filter_input(INPUT_GET, 'id', FILTER_SANITIZE_NUMBER_INT);
-    if (in_array($filters_type, array_column($posts_types, 'id'))) {
-        $sql = "SELECT post_date,
-        u.user_name,
-        u.avatar,
-        t.type_name,
-        t.icon_class,
-        p.id,
-        title,
-        content,
-        content_add,
-        link_icon,
-        views_total
-    FROM posts p
-    JOIN users u
-        ON p.user_id = u.id
-    JOIN posts_types t
-        ON p.type_id = t.id
-    WHERE p.type_id = '$filters_type'
-    ORDER BY views_total DESC";
-    } else {
+    if (!in_array($filters_type, array_column($posts_types, 'id'))) {
         $filters_type = NULL;
-        $sql = "SELECT post_date,
-        u.user_name,
-        u.avatar,
-        t.type_name,
-        t.icon_class,
-        p.id,
-        title,
-        content,
-        content_add,
-        link_icon,
-        views_total
-    FROM posts p
-    JOIN users u
-        ON p.user_id = u.id
-    JOIN posts_types t
-        ON p.type_id = t.id
-ORDER BY views_total DESC";
     }
-    if ($res = mysqli_query($con, $sql)) {
-        $popular_posts = mysqli_fetch_all($res, MYSQLI_ASSOC);
-        $posts_content = include_template('posts', [
-            'popular_posts' => $popular_posts
-        ]);
-        $page_content = include_template('popular-layout', [
-            'content' => $posts_content, 'posts_types' => $posts_types, 'filters_type' => $filters_type
-        ]);
-        $page_title = 'readme: популярное';
-        show_page($page_content, $page_title, $user_name);
-    } else {
-        show_error(true, $con, false);
-    }
+    $popular_posts = get_popular_posts($con, $filters_type);
+    $posts_content = include_template('posts', [
+        'popular_posts' => $popular_posts
+    ]);
+    $page_content = include_template('popular-layout', [
+        'content' => $posts_content, 'posts_types' => $posts_types, 'filters_type' => $filters_type
+    ]);
+    $page_title = 'readme: популярное';
+    show_page($page_content, $page_title, $user_name);
 } else {
     show_error(true, $con, false);
 }
--- post.php
+++ post.php
@@ -2,44 +2,17 @@
 require_once 'util/helpers.php';
 require_once 'tmp-data.php';
 require_once 'util/functions.php';
+require_once 'util/db_functions.php';
 require_once 'init.php';
 
 $post_id = (int) filter_input(INPUT_GET, 'id', FILTER_SANITIZE_NUMBER_INT);
 if ($post_id) {
-    $sql = "SELECT post_date,
-    u.user_name,
-    u.avatar,
-    u.reg_date,
-    t.type_name,
-    t.icon_class,
-    title,
-    content,
-    content_add,
-    link_icon,
-    views_total,
-    COUNT(s.id) AS subscr_total
-FROM posts p
-JOIN users u
-    ON p.user_id = u.id
-JOIN posts_types t
-    ON p.type_id = t.id
-JOIN subscribe s
-    ON p.user_id = s.tracked_id
-WHERE p.id = '$post_id'";
-    if ($res = mysqli_query($con, $sql)) {
-        $post = mysqli_fetch_array($res, MYSQLI_ASSOC);
-        if ($post['post_date']) {
-            $page_content = include_template('post-details', [
-                'post' => $post
-            ]);
-            $page_title = 'readme: пост';
-            show_page($page_content, $page_title, $user_name);
-        } else {
-            show_error(false, '404: Страница не существует', true);
-        }
-    } else {
-        show_error(true, $con, false);
-    }
+    $post = get_post($con, $post_id);
+    $page_content = include_template('post-details', [
+        'post' => $post
+    ]);
+    $page_title = 'readme: пост';
+    show_page($page_content, $page_title, $user_name);
 } else {
     show_error(false, '404: Страница не существует', true);
 }
